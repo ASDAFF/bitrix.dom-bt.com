12ecf03c7aa2590dd9421601b13edb0f7fe51d28
Michael Nossov <webseo.kz@gmail.com>
2018-11-09 00:47:31 +0500

    Сделал свои иконки соцсетей и разместил в подвале сайта

--- bitrix/templates/simplestroysite_copy/components/bitrix/news.list/smt_social/template.php
+++ bitrix/templates/simplestroysite_copy/components/bitrix/news.list/smt_social/template.php
@@ -12,8 +12,8 @@
 /** @var CBitrixComponent $component */
 $this->setFrameMode(true);
 ?>
-<?if(!empty($arResult["ITEMS"])):?>
-<ul class="list-inline smt-social">
+<?if(!empty($arResult["ITEMS"]) && (1==2)): //закрыл стандартный блок соцсетей, чтобы вывести свои стилизованные облегчённые иконки  ?>
+<ul class="list-inline smt-social ws1">
 <?foreach($arResult["ITEMS"] as $arItem):?>
 <?
 $this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
@@ -25,3 +25,57 @@ $this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayB
 <?endforeach;?>
 </ul>
 <?endif;?>
+<ul class="list-inline smt-social">
+    <li>
+        <a class="smt-social__icon" href="https://vk.com/public173408980" target="_blank" rel="nofollow noopener">
+            <img src="/smt_images/social_icons_ws4.svg#vk" alt="VKontakte" style="width: 32px;">
+        </a>
+    </li>
+    <li>
+        <a class="smt-social__icon" href="https://www.facebook.com/EvropaDom/" target="_blank" rel="nofollow noopener">
+            <img src="/smt_images/social_icons_ws4.svg#fb" alt="Facebook" style="width: 32px;">
+        </a>
+    </li>
+    <li>
+        <a class="smt-social__icon" href="https://www.instagram.com/evropeiskiidom/" target="_blank" rel="nofollow noopener">
+            <img src="/smt_images/social_icons_ws4.svg#insta" alt="Instagram" style="width: 32px;">
+        </a>
+    </li>
+    <li>
+        <a class="smt-social__icon" href="https://www.youtube.com/c/%D0%95%D0%B2%D1%80%D0%BE%D0%BF%D0%B5%D0%B9%D1%81%D0%BA%D0%B8%D0%B9%D0%94%D0%BE%D0%BC" target="_blank" rel="nofollow noopener">
+            <img src="/smt_images/social_icons_ws4.svg#youtube" alt="Youtube" style="width: 32px;">
+        </a>
+    </li>
+    <li>
+        <a class="smt-social__icon" href="https://twitter.com/evropeyskiydom" target="_blank" rel="nofollow noopener">
+            <img src="/smt_images/social_icons_ws4.svg#twitter" alt="twitter" style="width: 32px;">
+        </a>
+    </li>
+</ul>
+<ul class="list-inline smt-social">
+    <li>
+        <a class="smt-social__icon" href="https://ok.ru/group/55808558039065" target="_blank" rel="nofollow noopener">
+            <img src="/smt_images/social_icons_ws4.svg#ok" alt="Одноклассники" style="width: 32px;">
+        </a>
+    </li>
+    <li>
+        <a class="smt-social__icon" href="https://plus.google.com/u/0/communities/104308811347148379402" target="_blank" rel="nofollow noopener">
+            <img src="/smt_images/social_icons_ws4.svg#g-plus" alt="Google+" style="width: 32px;">
+        </a>
+    </li>
+    <li>
+        <a class="smt-social__icon" href="https://my.mail.ru/community/europadom/" target="_blank" rel="nofollow noopener">
+            <img src="/smt_images/social_icons_ws4.svg#mir" alt="Мой Мир" style="width: 32px;">
+        </a>
+    </li>
+    <li>
+        <a class="smt-social__icon" href="https://www.pinterest.ru/dombtcom/" target="_blank" rel="nofollow noopener">
+            <img src="/smt_images/social_icons_ws4.svg#pinterest" alt="Pinterest" style="width: 32px;">
+        </a>
+    </li>
+    <li>
+        <a class="smt-social__icon" href="#" target="_blank" rel="nofollow noopener">
+            <img src="/smt_images/social_icons_ws4.svg#linkedin" alt="LinkedIn" style="width: 32px;">
+        </a>
+    </li>
+</ul>
